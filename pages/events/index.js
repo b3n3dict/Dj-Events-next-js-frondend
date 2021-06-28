b5de3f17faa4c86d0eae004dc856b68b2f091d33
Benedict Xavier <benedict.xavier.e@gmail.com>
2021-06-28 21:26:49 +0530

    event api routes(temp)

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,11 +1,26 @@
-import Layout from "@/components/Layout"
+import Layout from '@/components/Layout'
+// import EventItem from '@/components/EventItem'
+import { API_URL } from '@/config/index'
 
-const index = () => {
-    return (
-        <Layout title="Dj events">
-            <h1>My events</h1>
-        </Layout>
-    )
+export default function EventsPage({ events }) {
+  return (
+    <Layout>
+      <h1>Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+
+      {/* {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))} */}
+    </Layout>
+  )
 }
 
-export default index
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+
+  return {
+    props: { events },
+    revalidate: 1,
+  }
+}
