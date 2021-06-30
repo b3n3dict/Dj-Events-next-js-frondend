b8815f13f5a9f1f307a6f83f3ebd02fca39fc12d
Benedict Xavier <benedict.xavier.e@gmail.com>
2021-07-01 02:12:44 +0530

    search bar and search funtionality added

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,23 @@
 import Link from 'next/link'
 import styles from '@/styles/Header.module.css'
+import Image from 'next/image'
+import { FaSearch } from 'react-icons/fa'
+import SearchBar from './SearchBar'
+
 export default function Header() {
     return (
         <header className={styles.header}>
          <div className={styles.logo}>
+        
              <Link href="/">
+             <div className={styles.logo_div}>
+             <img className={styles.log_img} src="https://i.pinimg.com/originals/05/4a/a3/054aa3421c22e0c9e04ada3082066a8d.gif"/>
                  <a>Dj Events</a>
+             </div>
              </Link>
          </div>
+             
+           <SearchBar/>
             <nav>
                 <ul>
                     <li>
@@ -15,6 +25,12 @@ export default function Header() {
                         <a>Events</a>
                     </Link>
                     </li>
+                    <li>
+                    <Link href="/events">
+                        <a>Events</a>
+                    </Link>
+                    </li>
+                   
                 </ul>
             </nav>
         </header>
---
+++ components/SearchBar.js
@@ -0,0 +1,24 @@
+import styles from '@/styles/Search.module.css'
+import { FaSearch } from 'react-icons/fa'
+import { useState } from 'react'
+import { useRouter } from 'next/router'
+
+const SearchBar = () => {
+  const router = useRouter()
+   const [value,setValue] = useState('')
+    const onSubmitHandler=(e)=>{
+       e.preventDefault()
+       router.push(`/events/search?term=${value}`)
+    }
+    return (
+        <form onSubmit={onSubmitHandler}>
+        <div className={styles.searchbar}>
+        <input className={styles.search_input} value={value} type="text" name="" placeholder="Search.." onChange={(e)=>setValue(e.target.value)}/>          
+          <button type="submit" className={styles.search_icon}> <FaSearch /></button>
+          
+      </div>
+      </form>
+    )
+}
+
+export default SearchBar
