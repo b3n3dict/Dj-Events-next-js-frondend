f265dd8f6271f4a322dd7f342bf98899db18f0cc
Benedict Xavier <benedict.xavier.e@gmail.com>
2021-07-04 18:31:46 +0530

    store jwt in httpOnly cookie

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,5 +1,5 @@
 import {API_URL} from '@/config/index'
-
+import cookie from 'cookie'
 export default async (req,res)=>{
      if(req.method === 'POST'){
          const {identifier,password} = req.body
@@ -12,8 +12,18 @@ export default async (req,res)=>{
              body: JSON.stringify({identifier,password})
          })
          const data = await strapiRes.json()
-         if(strapiRes.ok){
-            //  set The cookie 
+         if (strapiRes.ok) {
+            // Set Cookie
+            res.setHeader(
+              'Set-Cookie',
+              cookie.serialize('token', data.jwt, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV !== 'development',
+                maxAge: 60 * 60 * 24 * 7, // 1 week
+                sameSite: 'strict',
+                path: '/',
+              })
+            )
             res.status(200).json({user:data.user,})
          }else{
              res.status(data.statusCode).json({message:data.message[0].messages[0].message})
