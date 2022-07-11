bb8a895dae45ac274424831b512c438d084572d1
Benedict <benedict.xavier.e@gmail.com>
2022-07-11 08:02:57 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import styles from '@/styles/Footer.module.css'
 const Footer = () => {
     return (
         <footer className={styles.footer}>
-             <p>Copyright &copy; DJ Events 2021</p>
+             <p>Copyright &copy; DJ Events 2019</p>
              <p>
                  <Link href='/about'>About This Project </Link>
              </p>
